afd1786f789865fb786de8bac869e0c88df84676
yakumioto <yaku.mioto@gmail.com>
2020-06-08 15:14:19 +0800

    Updated: 2020-06-08 15:14:19
    Signed-off-by: yakumioto <yaku.mioto@gmail.com>

--- example/viper/example_test.go
+++ example/viper/example_test.go
@@ -2,6 +2,8 @@ package viper
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -32,4 +34,35 @@ func ExampleAddConfigPath() {
 	fmt.Println(viper.GetString("hello"))
 	// output:
 	// world
+}
+
+// ExampleAutomaticEnv
+// 设置 Automatic env 使用 Get 相关的方法会依次查询, env 的优先级高. 所以会覆盖参数.
+// 查询环境变量时需要使用 '_', 如果想使用 '.' 需要使用 SetEnvKeyReplacer 进行替换.
+func ExampleAutomaticEnv() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./etc/viper")
+	viper.ConfigFileUsed()
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	_ = os.Setenv("HELLO", "emmm")
+	fmt.Println(viper.GetString("hello"))
+	fmt.Println(viper.GetString("o1.o2.hello"))
+	_ = os.Setenv("O1_O2_HELLO", "emmm")
+	fmt.Println(viper.GetString("o1.o2.hello"))
+	fmt.Println(viper.GetString("o1_o2_hello"))
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	fmt.Println(viper.GetString("o1.o2.hello"))
+	// output:
+	// emmm
+	// world
+	// world
+	// emmm
+	// emmm
 }
